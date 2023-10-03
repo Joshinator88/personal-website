d45125a58e2eede9d5ad56c6176e90e972321a64
Joshua Bruijn <joshuaraatig@hotmail.com>
2023-10-03 11:43:50 +0200

    basics of the website

---
+++ index.php
@@ -0,0 +1,94 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet" type="text/css">
+    <script src="./script.js" defer></script>
+    <title>Joshua de Bruijn</title>
+</head>
+<body>
+
+<header>
+<img src="" alt="Logo of my website">
+    <nav>
+        <div class="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+        <ul>
+            <!-- the blog is on another page but the about me and project links scroll down to the specific sections -->
+            <li><a href="blog.php">Blog</a></li>
+            <li>About me</li>
+            <li>Projects</li>
+            <li>
+                <a href="https://github.com/Joshinator88">
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0 0 30 30">
+                        <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
+                    </svg>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/in/joshua-de-bruijn-a81347202/">
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0 0 48 48">
+                        <path fill="#0288D1" d="M42,37c0,2.762-2.238,5-5,5H11c-2.761,0-5-2.238-5-5V11c0-2.762,2.239-5,5-5h26c2.762,0,5,2.238,5,5V37z"></path><path fill="#FFF" d="M12 19H17V36H12zM14.485 17h-.028C12.965 17 12 15.888 12 14.499 12 13.08 12.995 12 14.514 12c1.521 0 2.458 1.08 2.486 2.499C17 15.887 16.035 17 14.485 17zM36 36h-5v-9.099c0-2.198-1.225-3.698-3.192-3.698-1.501 0-2.313 1.012-2.707 1.99C24.957 25.543 25 26.511 25 27v9h-5V19h5v2.616C25.721 20.5 26.85 19 29.738 19c3.578 0 6.261 2.25 6.261 7.274L36 36 36 36z"></path>
+                    </svg>
+                </a>
+            </li>
+            <li>
+                <a href="mailto:joshuaraatig@hotmail.com">
+                    <svg xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="40" height="40" viewBox="0 0 48 48">
+                        <path fill="#1976d2" d="M28,13h14.533C43.343,13,44,13.657,44,14.467v19.066C44,34.343,43.343,35,42.533,35H28V13z"></path><rect width="14" height="15.542" x="28" y="17.958" fill="#fff"></rect><polygon fill="#1976d2" points="27,44 4,39.5 4,8.5 27,4"></polygon><path fill="#fff" d="M15.25,16.5c-3.176,0-5.75,3.358-5.75,7.5s2.574,7.5,5.75,7.5S21,28.142,21,24	S18.426,16.5,15.25,16.5z M15,28.5c-1.657,0-3-2.015-3-4.5s1.343-4.5,3-4.5s3,2.015,3,4.5S16.657,28.5,15,28.5z"></path><rect width="2.7" height="2.9" x="28.047" y="29.737" fill="#1976d2"></rect><rect width="2.7" height="2.9" x="31.448" y="29.737" fill="#1976d2"></rect><rect width="2.7" height="2.9" x="34.849" y="29.737" fill="#1976d2"></rect><rect width="2.7" height="2.9" x="28.047" y="26.159" fill="#1976d2"></rect><rect width="2.7" height="2.9" x="31.448" y="26.159" fill="#1976d2"></rect><rect width="2.7" height="2.9" x="34.849" y="26.159" fill="#1976d2"></rect><rect width="2.7" height="2.9" x="38.25" y="26.159" fill="#1976d2"></rect><rect width="2.7" height="2.9" x="28.047" y="22.706" fill="#1976d2"></rect><rect width="2.7" height="2.9" x="31.448" y="22.706" fill="#1976d2"></rect><rect width="2.7" height="2.9" x="34.849" y="22.706" fill="#1976d2"></rect><rect width="2.7" height="2.9" x="38.25" y="22.706" fill="#1976d2"></rect><rect width="2.7" height="2.9" x="31.448" y="19.112" fill="#1976d2"></rect><rect width="2.7" height="2.9" x="34.849" y="19.112" fill="#1976d2"></rect><rect width="2.7" height="2.9" x="38.25" y="19.112" fill="#1976d2"></rect>
+                    </svg>
+                </a>
+            </li>
+        </ul>
+    </nav>
+</header>
+    <main>
+        <!-- een sectie waarin ik mijzelf voorstel -->
+        <section class="aboutMe">
+            <div>
+                <h1>Joshua de Bruijn</h1>
+                <p>
+                    Mijn naam is Joshua, ik ben 23 jaar en ik zit in mijn eerste jaar van de opleiding hbo software developer.
+                    Voordat ik deze opleiding ben begonnen was ik al veel bezig met programmeren en heb dus al wat (web) applicaties geschreven en heb hiervoor de talen 
+                    C#, javascript, PHP, html en css gebruikt. en ook tijdens de opleiding ben ik nog veel bezig met mijn eigen projecten omdat ik het ontzettend leuk vind dat ik 
+                    mijn ideen werkelijkheid kan maken. Verder ben ik ook erg gepassioneerd over kickboksen mixed martial arts en judo. En wanneer er een lekker windje staat ben 
+                    ik of op het ijsselmeer aan het zeilen met mijn vriendin, of op het markermeer aan het surfen met mijn broertje.
+                </p>
+            </div>
+            <div>
+                <img src="" alt="a picture of me">
+            </div>
+        </section>
+
+        <!-- een sectie waarin ik mijn projecten laat zien -->
+        <section class="projects">
+            <h1>Projects</h1>
+
+            <article class="projectDiv">
+                <h1>Chats</h1>
+                <div class="content">
+                    <div class="projectPicture">
+                        <img src="./images/captureChats.PNG" alt="a preview of the project">
+                    </div>
+
+                    <div class="projectDescription">
+                        <p>
+                            chats is een applicatie waar mensen zich kunnen aanmelden en andere gebruikers kunnen opzoeken. dan kunnen zij deze gebruikers toevoegen
+                            als vriend, zodoende zal de verzochte gebruiker een bericht krijgen met de mogelijkheid het verzoek te accepteren of te negeren. Wanneer het
+                            verzoek is geaccepteerd staan de gebruikers in elkaars contact pagina en is het dus mogelijk om elkaar berichten te sturen. 
+                        </p>
+                    </div>
+                </div>
+                
+            </article>
+        </section>
+    </main>
+</body>
+</html>
